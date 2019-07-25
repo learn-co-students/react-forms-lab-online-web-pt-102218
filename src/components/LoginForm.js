f97813087a559114fcd84b7911edea08675092e2
Johnathon Wood <jaywood128@gmail.com>
2019-07-25 14:00:12 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,29 +1,47 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+  handleUsername = (event) => {
+    this.setState({
+    username: event.target.value 
+    })
+  }
+  handleFormPassword = (event) => {
+    this.setState({
+    password: event.target.value 
+    })
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    if (!this.state.username || !this.state.password) return 
+      this.props.handleLogin(this.state)
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={event => this.handleSubmit(event)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={this.handleUsername} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.password} onChange={this.handleFormPassword} />
           </label>
         </div>
         <div>
-          <button type="submit">Log in</button>
+          <button type="submit" value="Submit">Log in</button>
         </div>
       </form>
     );
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,28 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      message: '', 
+      maxTweet: 140 
+    };
+  }
+  handleMessageChange = (event) => {
+    const updatedTweet = this.state.maxTweet - 1
+    this.setState({
+      message: event.target.value,
+      maxTweet: updatedTweet
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={event => this.handleMessageChange(event)} value={this.state.message}/>
+        <h2> {this.state.maxTweet}</h2> 
       </div>
     );
   }
